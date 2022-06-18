fb2979a7f81bd9bd2cd1db687708342d5d1c4c61
Ignacio Mansilla Derqui <ignacio.mansilla@qmenta.com>
2022-06-18 11:13:35 +0200

    Hand programming language test

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Funny Challenge
+
+[Exercise](https://github.com/jesus-seijas-sp/hand-challenge)
---
+++ test.js
@@ -0,0 +1,11 @@
+/* global test, expect */
+const translate = require('./index.js')
+
+test('it translate "Hello" accordingly', () => {
+  expect(translate('👇🤜👇👇👇👇👇👇👇👉👆👈🤛👉👇👊👇🤜👇👉👆👆👆👆👆👈🤛👉👆👆👊👆👆👆👆👆👆👆👊👊👆👆👆👊')).toBe('Hello')
+})
+
+test('it translate "Hello World!" accordingly', () => {
+  expect(
+    translate('👉👆👆👆👆👆👆👆👆🤜👇👈👆👆👆👆👆👆👆👆👆👉🤛👈👊👉👉👆👉👇🤜👆🤛👆👆👉👆👆👉👆👆👆🤜👉🤜👇👉👆👆👆👈👈👆👆👆👉🤛👈👈🤛👉👇👇👇👇👇👊👉👇👉👆👆👆👊👊👆👆👆👊👉👇👊👈👈👆🤜👉🤜👆👉👆🤛👉👉🤛👈👇👇👇👇👇👇👇👇👇👇👇👇👇👇👊👉👉👊👆👆👆👊👇👇👇👇👇👇👊👇👇👇👇👇👇👇👇👊👉👆👊👉👆👊')
+).toBe('Hello World!\n')})
